392aa8d43f8a3f35321d6faa01df86120f3ed7ac
sabuhihuseynov <huseynovsebuhi14@gmail.com>
2023-07-26 09:32:59 +0400

    delete unnecessary method

--- src/main/java/org/example/dao/repository/CityRepository.java
+++ src/main/java/org/example/dao/repository/CityRepository.java
@@ -8,9 +8,6 @@ import java.util.List;
 
 @Repository
 public interface CityRepository extends JpaRepository<City, Long> {
-
-    List<City> findAll();
-
     List<City> findCitiesByCountryId(Long countryId);
 
 }
